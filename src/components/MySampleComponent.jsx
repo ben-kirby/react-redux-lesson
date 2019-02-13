f46c85251e9677f4418e7169e973af394f68a9b8
Robert Lee <bobbydwise1@hotmail.com>
2019-02-13 13:24:27 -0800

    Attempt to add image loader.

---
+++ src/components/MySampleComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MySampleImageComponent from '../assets/images/cutestpuppy.jpg';
+
+function MySampleComponent(){
+  return (
+    <div>
+      <img src={reallyAdorablePuppy}/>
+    </div>
+  );
+}
+
+export default MySampleComponent;
