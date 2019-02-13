f319c9bb990ba44995140e66e9d7afb26eaf2aa8
Robert Lee <bobbydwise1@hotmail.com>
2019-02-13 14:00:36 -0800

    Add another image to top of app component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,22 @@ import NewTicketForm from './NewTicketForm';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
 import MySampleImage from '../assets/images/testimage.png';
+import MyAnotherTestImage from '../assets/images/testwide.png';
 
 function App(){
+  const topLineImageStyle = {
+    margin: "0 auto 0 auto",
+    display: "inline-block",
+  }
   const mainImageStyle = {
     width: "25%",
     height: "auto"
   }
   return (
     <div>
-      <div>
+      <div style={topLineImageStyle}>
         <img src={MySampleImage} style={mainImageStyle}/>
+        <img src={MyAnotherTestImage} style={mainImageStyle}/>
       </div>
       <Header/>
       <Switch>
