df86086b922c635493bd9e7064e8b0b5d9e79ba3
Dan Burbach and Ryan Leslie <dan+ryan@>
2019-02-25 16:40:28 -0800

    fix error

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
-import { createStore } from 'redux';
-import ticketListReducer from './reducers/ticket-list-reducer';
-import { Provider } from 'react-redux';
 
 const store = createStore(ticketListReducer);
 const render = (Component) => {
@@ -13,7 +10,6 @@ const render = (Component) => {
     <AppContainer>
       <HashRouter>
         <Component/>
-         </Provider store={store}>
       </HashRouter>
     </AppContainer>,
     document.getElementById('react-app-root')
